c88c5594179b3bf185c4b411494f8ffbdcac30fc
Brian Daley <brian@uconn.edu>
2021-02-11 21:20:35 -0500

    more reliable with polling

---
+++ README.md
@@ -0,0 +1,10 @@
+# layout-highlighter.js poc
+A teaching aid that attempts to visually highlight flexbox layouts while coding them in the browser.
+
+## How to Use
+When running [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer), add the layout-hightligher.js script to the bottom of your page.
+
+### URLs
+Use the [jsDelivr](https://www.jsdelivr.com/) URLs for this repository. For example, the following URL would load the latest version of the script:
+
+https://cdn.jsdelivr.net/gh/uconndxlab/layout-highlighter/layout-highlighter.js
--- layout-highlighter.js
+++ layout-highlighter.js
@@ -11,6 +11,7 @@ let styles = `
 
     background-color: var(--flexbox-color);
     border: 1px solid var(--flexbox-color);
+    padding: var(--flexbox-padding);
 
 }`;
 
@@ -20,34 +21,22 @@ document.body.appendChild(css);
 
 
 let highlighterClassName = 'lh';
+let pollingInterval = 2000;
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Select the node that will be observed for mutations
-  const targetNode = document.querySelector('body');
-
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: false, childList: true, subtree: false, characterData: false, attributeOldValue: false };
-
-  // Callback function to execute when mutations are observed
-  const callback = function(mutationsList, observer) {
-      // Use traditional 'for loops' for IE 11
-      for(const mutation of mutationsList) {
-          if (mutation.type === 'childList' && mutation.addedNodes[0] instanceof Element) {
-              mutation.addedNodes.forEach(node => {
-                  if(getComputedStyle(node).display === 'flex'){
-                      node.classList.add(highlighterClassName)
-                  }else{
-                      node.classList.remove(highlighterClassName)
-                  }
-              })
-          }
-
-      }
-  };
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
-
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);  
-})
+/**
+ * Switched to setInterval to easily account for changes 
+ * to style due to media queries. Good for PoC, but 
+ * probably bad in the longrun.
+ */
+setInterval(() => {
+    let elements = document.querySelectorAll('body *');
+    elements.forEach(el => {
+        if(getComputedStyle(el).display === 'flex'){
+            // console.info('Highlighting:', el)
+            el.classList.add(highlighterClassName)
+        }else{
+            // console.info('Removing Highlighting:', el)
+            el.classList.remove(highlighterClassName)
+        }
+    });
+}, pollingInterval);
